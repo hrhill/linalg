46a0532d34e2fb116cc41abba41cc8ff6a9fcbdb
hrhill <hill.harry.r@gmail.com>
2015-11-02 13:15:16 +0000

    Add potrflogdet.

--- include/linalg/determinants.hpp
+++ include/linalg/determinants.hpp
@@ -23,7 +23,7 @@ double getrfdet(const Matrix& a, const std::vector<int>& ipiv)
 
 /// \brief Compute the determinant, assuming that potrf has been called on m.
 template <typename Matrix>
-double potrfdet(const Matrix& a)
+double potrflogdet(const Matrix& a)
 {
     double logd = 0;
     for (size_t i = 0; i < linalg::num_rows(a); ++i)
@@ -31,7 +31,14 @@ double potrfdet(const Matrix& a)
         logd += log(a(i, i));
     }
     // return the square since |A| = |L|^2
-    return exp(2.0 * logd);
+    return 2.0 * logd;
+}
+
+/// \brief Compute the determinant, assuming that potrf has been called on m.
+template <typename Matrix>
+double potrfdet(const Matrix& a)
+{
+    return exp(potrflogdet(a));
 }
 
 }
