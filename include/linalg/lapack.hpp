682340c00cc6779695a9bbcf376a4b0a61ae2b7f
hrhill <hill.harry.r@gmail.com>
2015-11-05 14:07:35 +0000

    Qualify num_rows call.

--- include/linalg/lapack.hpp
+++ include/linalg/lapack.hpp
@@ -11,7 +11,7 @@ namespace linalg{
 template <typename Matrix>
 void mksym(Matrix& a)
 {
-    for (size_t i = 0; i < num_rows(a); ++i)
+    for (size_t i = 0; i < linalg::num_rows(a); ++i)
     {
         for (size_t j = 0; j < i; ++j)
         {
